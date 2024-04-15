569e52c8452916fc42907c66a07acdf660936cb6
Mehdi Rezaee <mehdi_rezaee@umbc.edu>
2024-04-15 19:35:40 -0400

    added explanatory comments to functions

--- app.py
+++ app.py
@@ -3,56 +3,33 @@ import requests
 from bs4 import BeautifulSoup, NavigableString, Tag
 from flask_cors import CORS
 from nltk.tokenize import sent_tokenize
-from utils import correct_images_src,extract_content, extract_table
+from utils import correct_images_src,extract_content, extract_table_as_string
 from nltk.tokenize import sent_tokenize
 from llm_qa import find_answer
 import json
 from fuzzywuzzy import fuzz
+import re
 
+# Initialize Flask application and enable CORS
 app = Flask(__name__)
 CORS(app)
 
 # Route to serve the index.html page
 @app.route('/')
 def home():
+    """Serve the index.html as the home page."""
     return render_template('index.html')
 
-debug_candid = 'first, some biases and pressures seem to be present naturally, or universally, among different learning systems, including deep learning agents.'
-def debug_writing_tables(doc_name):
-    from utils import all_tables
-    #print(f'all_tables: {all_tables}')
-    with open('./{doc_name}_temp_tables.txt','w') as file:
-        for index,table in enumerate(all_tables):
-            table_index = index+1
-            file.write('\n')
-            file.write('\n')
-            file.write(f'index is {table_index}: ')
-            file.write('\n')
-            file.write(table)
-            file.write('\n')
-            file.write('*'*50)
-            print(table)
-            print('*'*50)
-
 def replace_with_highlight(element, candid, similarity_threshold=80):
+    """Highlight text within the element that matches the candidate text, based on a similarity threshold."""
     if isinstance(element, NavigableString):
         # Check similarity between the candidate text and the element'stext
         element_clean = element.text.strip().lower()
         element_senteces = sent_tokenize(element_clean)
-        #if candid == debug_candid:
-        #    print(f'str(element): {str(element).lower()}, ratio: {fuzz.ratio(candid, str(element))}')
-        ##    print(f'element_senteces: {element_senteces}')
-        #    print('+'*50)
-#
         for sentence in element_senteces:
             if fuzz.ratio(candid, sentence) > similarity_threshold:
                 # Highlight the whole text block since it's similar to the candidate
-                #modified_text = f'<span style="background-color: yellow;">{str(sentence)}</span>'
-
-                #print(f'sentence: {sentence}')
                 modified_text = str(element).lower().replace(sentence, f'<span style="background-color: yellow;">{sentence}</span>')
-                #print(f'modified_text: {modified_text}')
-                #print('*'*50)
                 new_soup = BeautifulSoup(modified_text, 'html.parser')
                 element.replace_with(new_soup)
 
@@ -65,45 +42,36 @@ def replace_with_highlight(element, candid, similarity_threshold=80):
 # Route to fetch content from a URL and highlight the specified sentence
 @app.route('/fetch_content', methods=['POST'])
 def fetch_content():
+    """Fetch and process content from the specified URL, and respond to queries provided in the request."""
+    # Extract details from the request
     data = request.get_json()
     url = data['url']
     question = data.get('question', '')
     index_type = data.get('index_type', '')
     metric_type = data.get('metric_type', '')
-    top_k = int(data.get('top_k', 10))
-    #llm_model = 'all-mpnet-base-v2'
+    if data.get('top_k'): 
+        top_k = int(data.get('top_k'))
+    else:
+        top_k = 10
     model_name = data.get('model_name', ' ')
 
-    print(f'url: {url}')
-    print(f'model_name: {model_name}')
-    print(f'question: {question}')
-    print(f'index_type: {index_type}')
-    print(f'metric_type: {metric_type}')
-    print(f'top_k: {top_k}')
-    #url = 'https://arxiv.org/html/2404.05567v1'
-    #question = 'what does Subfigure (a) show?'
-    #question = None
+    # Fetch the webpage content
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    #print(f'soup: {soup}')
+
+    # Correct image paths in the HTML
     correct_images_src(url,soup)
+
     if not question:
+        # If no question is asked, return the raw content
         return jsonify({"content": str(soup)})
+
+    # Extract and clean the text content from the HTML
     doc_name = url.split('/')[-1]
     content = extract_content(soup.find('body'))
-    #print(f'content: {content}')
     doc_texts_list = content.split('<sent>')
-    #print(f'doc_texts_list: {doc_texts_list}')
-    #debug_writing_tables(doc_name)
-    #exit()
-    print(f'url: {url}')
-    print(f'gathering content from {url}')
-    print(f'question: {question}')
-    print(f'some of sentences are ')
-    print(f'doc_name is {doc_name}')
-    samples = doc_texts_list[:10]
-    for sample in samples:
-        print(sample)
+
+    # Process the query using the specified language model
     logs_dict = find_answer(doc_texts_list, 
                             question,
                             doc_name,
@@ -114,45 +82,24 @@ def fetch_content():
                             use_cache = True,
                             write_log = False)
 
-    #print(f'logs_dict is {logs_dict}')
+    # Process the query using the specified language model
     results = logs_dict[0]['results']
     selected_answers = []
     highlight_sentences = []
     seen = set()
-    #counter = 1
     for item in results:
         if item['text'] not in seen and len(item['text'])>1:
-            #selected_answers.append(' '.join([str(counter),'- ',item['text'],'score: '+str(item['score']),'\n']))
             seen.add(item['text'])
             selected_answers.append((item['text'],item['score']))
-            #highlight_sentences.append(item['text'])
-            #counter +=1 
-    #selected_answers = set([item['text'] for item in results if len(item)>1])
-    #selected_answers = list(selected_answers)
     selected_answers.sort(key=lambda x: x[1])
     highlight_sentences = [item[0] for item in selected_answers]
     for candid in highlight_sentences: 
-        print(f'candid: {candid}')
         try: 
-            print(f'replacing with {candid}')
             replace_with_highlight(soup,candid)
-            print(f'replacing with {candid} done!!!')
-            print('+'*50)
         except:
             print('couldnt replace with {candid}')
 
-    #print(json.dumps(results_list,indent=4))
-    '''
-    for index,sent in enumerate(splited_sents):
-        print(f'{index}- {sent}')
-    '''
-    #print('https://arxiv.org/html/2404.05567v1')
-
-    #if question:
-    #    print(f'selected_answers: {selected_answers}')
-    #    replace_with_highlight(soup,selected_answers)
-
-    #box_answers = [str(counter+1)+'- '+item[0]+' score: '+str(item[1]) for counter,item in enumerate(selected_answers)]
+    # Prepare a list of answers for display
     box_answers = [str(counter//2+1)+'- '+selected_answers[counter//2][0]+' score: '+str(selected_answers[counter//2][1]) if counter%2==0 else " " for counter in range(2*len(selected_answers))]
     return jsonify({"content": str(soup), "answers": box_answers})
 
--- llm_qa.py
+++ llm_qa.py
@@ -7,16 +7,18 @@ from sentence_transformers import SentenceTransformer
 from pymilvus import Collection, CollectionSchema, FieldSchema, DataType, connections, utility
 import numpy as np
 from utils import MODEL_EMBEDDING_DIMENSIONS
-import json
-import argparse
-from collections import defaultdict
-import pandas as pd
-from sentence_transformers import SentenceTransformer
-from pymilvus import Collection, CollectionSchema, FieldSchema, DataType, connections, utility
 from numpy import save
 
 def connect_to_milvus(embed_dim):
-    """Connect to the Milvus server and create a collection for storing document embeddings."""
+    """
+    Connect to the Milvus server and create a collection for storing document embeddings.
+    
+    Args:
+        embed_dim (int): The dimension of the embeddings.
+
+    Returns:
+        Collection: A Milvus Collection object for storing embeddings.
+    """
     connections.connect()
     fields = [
         FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
@@ -30,13 +32,32 @@ def connect_to_milvus(embed_dim):
     return collection
 
 def encode_text(model, texts, text_type):
-    """Encode text into embeddings using a pre-trained model."""
+    """
+    Encode text into embeddings using a pre-trained Sentence Transformer model.
+    
+    Args:
+        model (SentenceTransformer): The pre-trained Sentence Transformer model.
+        texts (list of str): Texts to be encoded.
+        text_type (str): A descriptive type label for logging purposes.
+
+    Returns:
+        np.ndarray: An array of embeddings.
+    """
     print(f'Encoding {text_type} to embeddings...')
     embeddings = model.encode(texts, convert_to_tensor=True)
     return embeddings
 
-def create_and_load_index(collection, index_type="IVF_FLAT", metric_type="L2"):
-    """Create and load an index for the Milvus collection."""
+def create_and_load_index(collection,
+                          index_type="IVF_FLAT",
+                          metric_type="L2"):
+    """
+    Create and load an index for the Milvus collection to optimize search operations.
+    
+    Args:
+        collection (Collection): The Milvus collection to create the index on.
+        index_type (str): The type of index to create.
+        metric_type (str): The metric type to use with the index.
+    """
     index_params = {
         "metric_type": metric_type,
         "index_type": index_type,
@@ -56,8 +77,32 @@ def search_similar_texts(collection,
                          index_type="IVF_FLAT",
                          metric_type="L2",
                          search_params=None,
+                         probe_params = None,
                          output_file_path=None):
-    """Search for texts similar to the query embeddings in the Milvus collection."""
+    """
+    Searches for texts in the Milvus collection that are similar to the 
+    query represented by the question_embedding.
+
+    Args:
+        collection (Collection): The Milvus collection where the documents are stored.
+        question_embedding (ndarray): The embedding vector of the query question.
+        doc_texts (list of str): List of document texts available in the collection.
+        id_to_index_mapping (dict): A mapping from Milvus returned ids to the document 
+        indices in doc_texts.
+        doc_question (str): The original text of the query question.
+        use_cache (bool): Flag to determine whether to use cached data or not.
+        write_log (bool): If True, search results will be written to a log file specified
+        by output_file_path.
+        top_k (int): Number of top results to retrieve in the search.
+        index_type (str): The type of index used in the Milvus collection for searching.
+        metric_type (str): The metric type used in the search index (e.g., L2, cosine).
+        search_params (dict, optional): Additional search parameters for Milvus. 
+        probe_params (dict, optional): Parameters for search probing, unused if None.
+        output_file_path (str, optional): Path to the file where results should be logged.
+
+    Returns:
+        list: A list containing dictionaries of search results with the matching texts and their scores.
+    """
 
     # Create and load index with the specified index type and parameters
     create_and_load_index(collection, index_type=index_type, metric_type=metric_type)
@@ -65,13 +110,10 @@ def search_similar_texts(collection,
     # Set default search parameters if none are provided
     if search_params is None:
         search_params = {"metric_type": metric_type, "params": {"nprobe": 10}}
-        #search_params = {"metric_type": metric_type}
 
     results_list = []
 
     # Perform search with the specified search parameters
-    #for i, query_embedding in enumerate(query_embeddings):
-    #if True:
     query_result = {
         "query": doc_question,
         "top_k": top_k,
@@ -101,8 +143,6 @@ def search_similar_texts(collection,
     results_list.append(query_result)
 
     # Write the results to a JSONLines file
-    print(f'writing the results in {output_file_path}')
-    print(f'results_list: {results_list}')
     if write_log:
         with open(output_file_path, 'w') as output_file:
             for result in results_list:
@@ -110,6 +150,32 @@ def search_similar_texts(collection,
                 output_file.write(json.dumps(result) + '\n')
     return results_list
 
+def prepare_output_paths(doc_name,
+                         model_name,
+                         index_type,
+                         metric_type,
+                         search_params):
+    """
+    Generates and prepares file paths for storing results and embeddings.
+
+    Args:
+        doc_name (str): Name of the document or dataset being processed.
+        model_name (str): Name of the Sentence Transformer model used for generating embeddings.
+        index_type (str): Type of index used in the Milvus search.
+        metric_type (str): Metric type used for the index in Milvus.
+        search_params (dict): Search parameters used in Milvus search.
+
+    Returns:
+        tuple: A tuple containing the path for the output file and the output tensor file.
+    """
+    config_str = f"{doc_name}_{index_type}_{metric_type}_params{str(search_params).replace(' ', '').replace(':', '')}"
+    output_folder = os.path.join("logs", config_str)
+    os.makedirs(output_folder, exist_ok=True)
+    output_file_path = os.path.join(output_folder, f"{doc_name}_{model_name}_{config_str}.json")
+    output_tensors_path = os.path.join(os.getcwd(), output_folder, f"{doc_name}_{model_name}_{config_str}.npy")
+    return output_file_path, output_tensors_path
+
+# Example of how to use the function in your main code:
 def find_answer(doc_texts_list,
                 question,
                 doc_name,
@@ -120,31 +186,51 @@ def find_answer(doc_texts_list,
                 index_type='IVF_FLAT',
                 metric_type='L2',
                 search_params=None):
-    """Main function to process the document and questions."""
-    print(f'find_answer model_name: {model_name}')
+    """
+    Processes the document and questions using embeddings, managing their storage,
+    retrieval, and searching within the Milvus collection. It involves encoding texts,
+    saving configurations, and results in a structured format in the 'logs' folder.
+
+    Args:
+        doc_texts_list (list): List of document texts to process.
+        question (str): Question string whose answer is to be found in the document texts.
+        doc_name (str): Name of the document being processed, used for naming files and logs.
+        model_name (str): Name of the Sentence Transformer model used for generating embeddings.
+        use_cache (bool): Flag to determine whether to use cached embeddings or generate new ones.
+        write_log (bool): Flag to enable logging of the search results to a file.
+        top_k (int): Number of top results to return from the search.
+        index_type (str): Type of index used in Milvus for searching embeddings.
+        metric_type (str): Metric type used in Milvus for the index.
+        search_params (dict, optional): Additional search parameters for Milvus. Defaults to None.
+
+    Returns:
+        list: A list containing the search results with documents that best match the question.
+    """
+
+
+
+    # Prepare paths for output files and embeddings
+    output_file_path, output_tensors_path = prepare_output_paths(doc_name, model_name,
+                                                                 index_type, metric_type,
+                                                                 search_params)
+
+    model = SentenceTransformer(model_name)
     embed_dim = MODEL_EMBEDDING_DIMENSIONS[model_name]
     collection = connect_to_milvus(embed_dim)
-    config_str = f"{doc_name}_{index_type}_{metric_type}_params{str(search_params).replace(' ', '').replace(':', '')}"
-    output_folder = os.path.join("logs", config_str)
-    os.makedirs(output_folder, exist_ok=True)
-    output_file_path = os.path.join(output_folder, f"{doc_name}_{model_name}_{config_str}.json")
-    output_tensors_path = os.path.join(os.getcwd(),output_folder, f"{doc_name}_{model_name}_{config_str}.npy")
-    print(f'loading the {model_name}')
-    model = SentenceTransformer(model_name)
-    print(f'use_cache: {use_cache}')
-    print(f'{output_tensors_path} exists: {os.path.exists(output_tensors_path)}')
+
+
     if not use_cache or not os.path.exists(output_tensors_path):
         text_embeddings = encode_text(model, doc_texts_list, 'sentences')
         text_embeddings_npy = text_embeddings.cpu().numpy()
         text_embeddings_list = text_embeddings.tolist()
-        print(f'text_embeddings: {text_embeddings}, type: {type(text_embeddings)}')
-        print(f'**** saving embeddings to {output_tensors_path} *****')
         save(output_tensors_path,text_embeddings_npy)
         #mr = collection.insert([text_embeddings.tolist()])
     else:
-        print('loading from {output_tensors_path}')
+        print(f'loading from {output_tensors_path}')
         text_embeddings_npy = np.load(output_tensors_path)
         text_embeddings_list = text_embeddings_npy.tolist()
+
+
     mr = collection.insert([text_embeddings_list])
     id_to_index_mapping = {id: index for index, id in enumerate(mr.primary_keys)}
     question_embedding = encode_text(model, question, 'question')
